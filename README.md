423738c920c4978e22577c09a2b64ec4312256ca
zero88 <sontt246@gmail.com>
2023-07-20 10:34:51 +0700

    Reset docs/main branch

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Beanx
-  
+
+- `docs/main`: Branch to keep Antora component documentation
+- `gh-pages`: Branch to host GitHub page
--- core/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-oss {
-    baseName.set("beanx")
-    title.set("Bean.x")
-}
-
-dependencies {
-    api(VertxLibs.core)
-    api(ValidationLibs.api)
-}
